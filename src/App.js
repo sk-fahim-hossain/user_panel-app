36eeb029cf2769a019c0afd79646e3dc56a4334a
sk-fahim-hossain <fahimhossainjob1@gmail.com>
2022-01-24 11:29:56 +0600

    this is a simple api call practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const names = [{name:"jowel",age:"12"}, {name:"fahim",age:"35"},{name :'rahim',age:"24"}]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Users></Users>
+
+    {
+      names.map(fnd =><li>{fnd.name}</li>)
+    }
+
+
+    {
+      names.map(fnd =><Friend name={fnd.name} age={fnd.age}></Friend>)
+    }
+
+   
+
+   <Friend></Friend>
+
     </div>
   );
+}
+
+function Friend(props){
+  const [nayok, setNayok] = useState([])
+  useEffect(() => {
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => setNayok(data))
+  },[])
+
+
+  const style ={
+    border: '1px solid lightgrey',
+    borderRadius: '5px',
+    width: '200px',
+    height: '200px',
+    backgroundColor: 'white',
+    padding: '100px auto',
+    margin: '20px'
+  }
+  return (
+    <div style={style}>
+      <h3>Name:{props.name}</h3>
+      <h4>Age:{props.age}</h4>
+      
+    </div>
+  )
+}
+
+function Users() {
+  const [user, setUser] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUser(data))
+  },[])
+  return (
+    <div>
+      <h2>Dynamic Users</h2>
+      <h4>Count:{user.length}</h4>
+      {
+        user.map(singleUser =>
+          <div>
+          <p><b>{singleUser.name}</b></p>
+          <p>Email:{singleUser.email}</p>
+          </div>
+        )
+      }
+    </div>
+  )
 }
 
 export default App;
